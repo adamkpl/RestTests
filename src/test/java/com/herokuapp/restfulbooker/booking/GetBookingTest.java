54d1fac17f5c11be0b5e6180e317e9fe424d5563
adamkpl <50586694+adamkpl@users.noreply.github.com>
2020-12-20 14:36:54 +0100

    Implement soft assertions in GetBookingTest
    Implemented with junit.rules.ErrorCollector.

--- src/test/java/com/herokuapp/restfulbooker/booking/GetBookingTest.java
+++ src/test/java/com/herokuapp/restfulbooker/booking/GetBookingTest.java
@@ -9,60 +9,68 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.hamcrest.Matchers;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import static org.hamcrest.Matchers.is;
 
 public class GetBookingTest extends BaseTestCase {
 
-    private static Response response;
+    private static Booking payload;
+    private static Response responsePost;
+    private static Integer responsePostedId;
+    private static Response responseGet;
+
+    @Rule
+    public ErrorCollector errorCollector = new ErrorCollector();
 
     @BeforeClass
     public static void setupTest() {
         // GIVEN Prepare & POST payload
-        response = HttpMethod.get(Endpoint.MAIN_URL + Endpoint.BOOKING,
-                HttpMethod.post(TestData.generateDefaultBookingPayload(),
-                        Endpoint.MAIN_URL + Endpoint.BOOKING).jsonPath().getInt(Booking.BOOKING_ID_KEY_NAME));
+        payload = TestData.generateDefaultBookingPayload();
+        responsePost = HttpMethod.post(payload, Endpoint.MAIN_URL + Endpoint.BOOKING);
+        responsePostedId = responsePost.jsonPath().getInt(Booking.BOOKING_ID_KEY_NAME);
+        responseGet = HttpMethod.get(Endpoint.MAIN_URL + Endpoint.BOOKING, responsePostedId);
     }
 
     @Test
     public void shouldBeContentTypeJson() {
         // THEN Assert that the format of payload received is application/json
-        response.then()
-                    .assertThat()
-                    .contentType(ContentType.JSON);
+        responseGet.then()
+                .assertThat()
+                .contentType(ContentType.JSON);
     }
 
     @Test
     public void shouldGetResponseInLessThan3seconds() {
         // THEN get response time in max. 3 seconds
-        response.then()
-                    .assertThat()
-                    .time(Matchers.lessThan(3000L));
+        responseGet.then()
+                .assertThat()
+                .time(Matchers.lessThan(3000L));
     }
 
     @Test
     public void shouldRespondOKWhenGetResponse() {
         // THEN Return Status Code = 200 OK
-        response.then()
-                    .assertThat()
-                    .statusCode(StatusCode.SC_OK);
+        responseGet.then()
+                .assertThat()
+                .statusCode(StatusCode.SC_OK);
     }
 
     @Test
     public void shouldGetCreatedPayloadAndVerifyAllValuesInsertedCorrectly() {
         // THEN Verify payload's key-values pairs if all were inserted as expected
-        response.then()
-                    .assertThat()
-                    .body(Booking.FIRST_NAME_KEY_NAME, is(TestData.FIRST_NAME))
-                    .body(Booking.LAST_NAME_KEY_NAME, is(TestData.LAST_NAME))
-                    .body(Booking.TOTAL_PRICE_KEY_NAME, is(TestData.TOTAL_PRICE))
-                    .body(Booking.DEPOSIT_PAID_KEY_NAME, is(TestData.DEPOSIT_PAID))
-                    .body(Booking.BOOKING_DATES_KEY_NAME + "." + Booking.BOOKING_DATES_CHECK_IN_KEY_NAME,
-                            is(TestData.BOOKING_DATES_CHECK_IN))
-                    .body(Booking.BOOKING_DATES_KEY_NAME + "." + Booking.BOOKING_DATES_CHECK_OUT_KEY_NAME,
-                            is(TestData.BOOKING_DATES_CHECK_OUT))
-                    .body(Booking.ADDITIONAL_NEEDS_KEY_NAME, is(TestData.ADDITIONAL_NEEDS));
+        Bookingid bookingPost = responsePost.as(Bookingid.class);
+        Booking bookingGet = responseGet.as(Booking.class);
+
+        errorCollector.checkThat(bookingPost.getBooking().getFirstname(), is(bookingGet.getFirstname()));
+        errorCollector.checkThat(bookingPost.getBooking().getLastname(), is(bookingGet.getLastname()));
+        errorCollector.checkThat(bookingPost.getBooking().getTotalprice(), is(bookingGet.getTotalprice()));
+        errorCollector.checkThat(bookingPost.getBooking().getDepositpaid(), is(bookingGet.getDepositpaid()));
+        errorCollector.checkThat(bookingPost.getBooking().getBookingdates().getCheckin(), is(bookingGet.getBookingdates().getCheckin()));
+        errorCollector.checkThat(bookingPost.getBooking().getBookingdates().getCheckout(), is(bookingGet.getBookingdates().getCheckout()));
+        errorCollector.checkThat(bookingPost.getBooking().getAdditionalneeds(), is(bookingGet.getAdditionalneeds()));
     }
 
 }
